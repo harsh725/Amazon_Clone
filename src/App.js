fc83687b637d69aa133265e1f9cfe871f678b0cf
HARSH DHIMAN <harsh.dhiman725@gmail.com>
2021-07-20 21:22:09 +0530

    Checkout page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./Checkout";
 
 function App() {
   {
@@ -11,14 +12,13 @@ function App() {
   return (
     <Router>
       <div className="app">
+        <Header />
         <Switch>
           <Route path="/checkout">
-            <Header />
-            <h1> This is gonna be the checkout page</h1>
+            <Checkout />
           </Route>
 
           <Route path="/">
-            <Header />
             <Home />
           </Route>
         </Switch>
---
+++ src/Subtotal.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Subtotal.css";
+import CurrencyFormat from "react-currency-format";
+function Subtotal() {
+  return (
+    <div className="subtotal">
+      <h1>hey there</h1>
+    </div>
+  );
+}
+
+export default Subtotal;
