6949f6043dde1fc47256f313dd382846e01878d0
HARSH DHIMAN <harsh.dhiman725@gmail.com>
2021-07-23 02:04:22 +0530

    stripe payment gateway

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
 import Login from "./Login";
+import Payment from "./Payment";
 
 import { auth } from "./firebase";
-
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { useStateValue } from "./StateProvider";
-import Payment from "./Payment";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe(
+  "pk_test_51JG8J4SGZ14IOneZRSQsxBALhfdrWyPjI0IVKvDrUKezaZOKXXWHlJtrdbR9GiKcEgGh5lFCbgc0gAuhAQwJm93I00QdcG8MLD"
+);
 
 function App() {
   const [{}, dispatch] = useStateValue();
+
   useEffect(() => {
     //runs once when the app component boot up
     auth.onAuthStateChanged((authUser) => {
@@ -41,7 +47,9 @@ function App() {
           </Route>
           <Route path="/payment">
             <Header />
-            <Payment />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
           </Route>
           <Route path="/checkout">
             <Header />
